d98c8cc83f835a7082e6036c8346c5d129c296dd
Prashant Kumar <prashantdrew23@gmail.com>
2021-10-02 22:37:01 +0530

    nothing

--- src/graphql/resolvers/ImageUpload.ts
+++ src/graphql/resolvers/ImageUpload.ts
@@ -25,22 +25,14 @@ export class ImageUploadResolver {
             api_secret: process.env.CLOUDINARY_API_SECRET,
         });
 
-        console.log(photo.slice(0, 100));
-
-
         const result = await cloudinary.uploader.upload(photo, {
-            //here i chose to allow only jpg and png upload
             allowed_formats: ["jpg", "png"],
-            //generates a new id for each uploaded image
             public_id: "",
-            /*creates a folder called "your_folder_name" where images will be stored.
-            */
             folder: "test",
         })
 
         if (result.secure_url) {
-            console.log(result);
-            return await UserModel.findOneAndUpdate({ username }, { imageUrl: result.secure_url }).then((user) => {
+            return await UserModel.findOneAndUpdate({ username }, { imageUrl: result.secure_url }).then(() => {
                 return {
                     status: true,
                     message: "Uploaded Succesfully",
